b60a70a7a3c6de44393ef7be43453cc7388b118d
OscarD8 <oscardavison8@outlook.com>
2025-02-12 17:18:28 +0000

    [code] Array introduction - started

---
+++ code/part3/ArrayIntro.java
@@ -0,0 +1,15 @@
+package code.part3;
+
+import java.util.ArrayList;
+
+public class ArrayIntro
+{
+    public static void main(String[] args){
+        int[] numbers = new int[5];
+        numbers[0] = 3;
+        numbers[1] = 2;
+        numbers[2] = 6;
+        numbers[3] = -1;
+        numbers[4] = 5;
+    }
+}
